5857ad33e65550feacdcd80e43f034e5de123be4
Hugo Gabriel <125702007+HugoTGabriel@users.noreply.github.com>
2025-05-03 13:55:22 -0300

    Add files via upload

---
+++ Ficha RPG/Data_Sistemas.py
@@ -0,0 +1,23 @@
+SISTEMAS = {
+    'Dungeons and Dragons': {
+        'raças': [
+            'Anão', 'Elfo', 'Halfling', 'Humano',
+            'Draconato', 'Gnomo', 'Meio-Elfo', 'Meio-Orc', 'Tiefling'
+        ],
+        'classes': [
+            'Bárbaro', 'Bardo', 'Bruxo', 'Clérigo', 'Druida', 'Feiticeiro',
+            'Guerreiro', 'Ladino', 'Mago', 'Monge', 'Paladino', 'Patrulheiro'
+        ],
+        'atributos': [
+            'Força', 'Destreza', 'Constituição',
+            'Inteligência', 'Sabedoria', 'Carisma'
+        ],
+
+    },
+
+    'Call of Cthulhu': {
+    },
+
+    'Ordem Paranormal': {
+    }
+}
